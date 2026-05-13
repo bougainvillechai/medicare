6a9e9df961cf4460ac23c183153dd6b06456c448
bougainvillechai <kajallibi@gmail.com>
2026-05-14 01:48:24 +0300

    admin and doctor profiles authenticated

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -1,10 +1,24 @@
 import { StrictMode } from 'react'
+import { BrowserRouter } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import AdminContextProvider from './context/adminContext.jsx'
+import DoctorContextProvider from './context/doctorContext.jsx'
+import AppContextProvider from './context/appContext.jsx'
+
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+
+  <BrowserRouter>
+    <AdminContextProvider>
+      <DoctorContextProvider>
+        <AppContextProvider>
+          <App />
+        </AppContextProvider>
+      </DoctorContextProvider>
+    </AdminContextProvider>
+  </BrowserRouter>
+
 )
